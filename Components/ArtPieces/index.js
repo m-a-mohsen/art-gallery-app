52cebfdb1133e4870a1214c2d51502114de6ce56
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-14 17:41:34 +0100

    fetch complete

---
+++ Components/ArtPieces/index.js
@@ -0,0 +1,3 @@
+export default function ArtPieces({ image, title, artist }) {
+  return <></>;
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
+import useSWR from "swr";
+
 export default function HomePage() {
+  const { data, error, isLoading, mutate } = useSWR(
+    "https://example-apis.vercel.app/api/art"
+  );
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  console.log(data);
+
   return (
-    <div>
-      <h1>Hello from Next.js</h1>
-    </div>
+    <main>
+      <ArtPieces
+        image={data.imageSource}
+        title={data.name}
+        artist={data.artist}
+      />
+    </main>
   );
 }
