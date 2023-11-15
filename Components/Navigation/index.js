a26461d79f8c4eb9032f3c9a4f62246d626cffbd
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-15 14:16:26 +0100

    added nav

---
+++ Components/Navigation/index.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function Navigation() {
+  return (
+    <ul>
+      <li>
+        <Link href="/art-pieces">Pieces</Link>
+      </li>
+    </ul>
+  );
+}
