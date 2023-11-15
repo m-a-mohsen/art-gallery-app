ca6957608794156b4c37cdd093b6df49f9a3afea
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-11-15 11:10:30 +0100

    initial commits

---
+++ pages/spotlight.js
@@ -0,0 +1,8 @@
+import Spotlight from "@/Components/Spotlight/Spotlight";
+
+export default function spotlight() {
+    const spotlightArtPiece = 
+  return (
+      <Spotlight image={ imageUrl} title={ title } />
+  )
+}
