2f5288c3a1f7cd8da6c8cd1f7cb57e6bf57a9beb
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-15 10:59:37 +0100

    added ArtPiecesPreviewcomponent

--- Components/ArtPieces/index.js
+++ Components/ArtPieces/index.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
+import ArtPiecesPreview from "../ArtPiecesPreview";
 
-export default function ArtPieces({ data }) {
+export default function ArtPieces({ pieces }) {
   return (
     <>
-      {data.map(({ name, artist, imageSource, slug }) => (
-        <li key={slug}>
-          <div key={name}>{name}</div>
-          <div>{artist}</div>
-          <Image src={imageSource} alt={name} width={312} height={312} />
+      {pieces.map((piece) => (
+        <li key={piece.slug}>
+          <ArtPiecesPreview
+            title={piece.name}
+            image={piece.imageSource}
+            artist={piece.artist}
+          />
         </li>
       ))}
     </>
