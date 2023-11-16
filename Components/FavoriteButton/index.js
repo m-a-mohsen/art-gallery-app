386797121745dc4938a9c3d48a19ce5f37e45839
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-16 13:12:54 +0100

    fixed button

--- Components/FavoriteButton/index.js
+++ Components/FavoriteButton/index.js
@@ -1,12 +1,14 @@
-export default function FavoriteButton({ artPiecesInfo, onToggleFavorite }) {
-  console.log(onToggleFavorite);
-  const { isFav } = artPiecesInfo.find(
-    (info) => info.slug === artPiecesInfo.slug
-  ) ?? {
+export default function FavoriteButton({
+  artPiecesInfo,
+  onToggleFavorite,
+  slug,
+}) {
+  const { isFav } = artPiecesInfo.find((info) => info.slug === slug) ?? {
     isFav: false,
   };
+
   return (
-    <button type="button" onClick={() => onToggleFavorite(artPiecesInfo.slug)}>
+    <button type="button" onClick={() => onToggleFavorite(slug)}>
       {isFav ? "Remove From Favorites" : "Add To Favorites"}
     </button>
   );
