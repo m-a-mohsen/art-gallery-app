61dd3dc6130ef14b70bfc1a80910c8722949f4cf
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-16 11:29:44 +0100

    detail page loading on click

--- Components/ArtPiecesPreview/index.js
+++ Components/ArtPiecesPreview/index.js
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import FavoriteButton from "../FavoriteButton";
+import Link from "next/link";
 
 export default function ArtPiecesPreview({
   image,
   title,
   artist,
+  slug,
   onToggleFavorite,
   artPiecesInfo,
 }) {
   return (
     <>
-      <Image src={image} alt={title} width={312} height={312} />
+      <Link href={`/art-pieces/${slug}`}>
+        <Image src={image} alt={title} width={312} height={312} />
+      </Link>
       <div>{title}</div>
       <div>{artist}</div>
+
       <FavoriteButton
         artPiecesInfo={artPiecesInfo}
         onToggleFavorite={onToggleFavorite}
