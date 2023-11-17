3bcfb7847fa058565f51c064bb9a262bcee60f89
ragnamarie <ragna.steinboemer@gmail.com>
2023-11-17 15:26:08 +0100

    add spotlight test

--- Components/Spotlight/Spotlight.test.js
+++ Components/Spotlight/Spotlight.test.js
@@ -3,8 +3,31 @@ import userEvent from "@testing-library/user-event";
 import Spotlight from ".";
 
 test("renders a random image", () => {
-  render(<Spotlight artPiecesInfo={[]} onToggleFavorite={[]} slug={[]} />);
+  render(
+    <Spotlight
+      image={[]}
+      artist={[]}
+      slug={[]}
+      artPiecesInfo={[]}
+      onToggleFavorite={[]}
+    />
+  );
 
   const image = screen.getByRole("img");
   expect(image).toBeInTheDocument();
+});
+
+test("renders the name of the artist", () => {
+  render(
+    <Spotlight
+      image={[]}
+      artist={[]}
+      slug={[]}
+      artPiecesInfo={[]}
+      onToggleFavorite={[]}
+    />
+  );
+
+  const heading = screen.getByRole("heading");
+  expect(heading).toBeInTheDocument();
 });
